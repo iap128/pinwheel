3d0cc2855dd5482d93475449567e29ca60dc8e91
Ryan Hunter <Ryan.Hunter@nike.com>
2024-02-12 08:36:03 -0800

    Added station ID saving

--- src/api/getCurrentConditions.ts
+++ src/api/getCurrentConditions.ts
@@ -5,6 +5,7 @@ import { formUrlParams } from "./helpers";
 const currentConditionsAPI = 'https://api.weather.com/v2/pws/observations/current';
 
 export interface CurrentResponse {
+    neighborhood: string;
     humidity: number;
     imperial: {
         dewpt: number;
