3b98652f17038a68c4f969f7c143ba54ec3ba337
Ryan Hunter <Ryan.Hunter@nike.com>
2024-02-12 09:18:35 -0800

    Added setup page

---
+++ src/components/SetupPage.tsx
@@ -0,0 +1,32 @@
+import { Button, Input, Modal, Space, Typography } from "antd";
+import { FC, useContext, useState } from "react";
+import { setCookie } from "typescript-cookie";
+import { StationContext } from "../StationContext";
+
+interface Props {
+    isOpen: boolean;
+    setIsOpen: (isOpen: boolean) => void;
+}
+
+const SetupPage: FC<Props> = ({ isOpen, setIsOpen }) => {
+    const { setStationID } = useContext(StationContext);
+    const [id, setID] = useState('');
+
+    const save = () => {
+        setCookie('stationID', id);
+        setStationID(id);
+        setIsOpen(false);
+    };
+
+    return (
+        <Modal title='Welcome to Pinwheel' open={isOpen} footer={[]}>
+            <Space direction="vertical">
+                <Typography.Text>Enter a station ID to continue</Typography.Text>
+                <Input placeholder="Station ID" value={id} onChange={(e) => setID(e.target.value)} />
+                <Button type="primary" onClick={save}>Save</Button>
+            </Space>
+        </Modal>
+    )
+};
+
+export default SetupPage;
