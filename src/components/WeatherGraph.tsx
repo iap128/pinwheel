2dc81f6233d7d55f66a7c3262b5e73eb4e497f4d
iap128 <103337777+iap128@users.noreply.github.com>
2024-10-03 08:29:49 -0700

    Better graph (#3)
    * Added mui charts
    
    * Got graph working
    
    ---------
    
    Co-authored-by: Ryan Hunter <Ryan.Hunter@nike.com>

--- src/components/WeatherGraph.tsx
+++ src/components/WeatherGraph.tsx
@@ -1,10 +1,10 @@
 import { DownOutlined } from '@ant-design/icons';
+import { LineChart } from '@mui/x-charts';
 import { Button, Modal } from 'antd';
 import { FC, useState } from 'react';
-import { Area, AreaChart, CartesianGrid, Tooltip, XAxis, YAxis } from 'recharts';
 
 interface Props {
-  data: {name: string, value: number}[];
+  data: { name: string; value: number }[];
   title: string;
 }
 
@@ -13,27 +13,14 @@ const WeatherGraph: FC<Props> = ({ data, title }) => {
 
   return (
     <div>
-      <Button shape="circle" size="small" onClick={() => setOpen(true)} icon={<DownOutlined />} />
+      <Button shape='circle' size='small' onClick={() => setOpen(true)} icon={<DownOutlined />} />
       <Modal title={title} open={open} onCancel={() => setOpen(false)} footer={[]}>
-        <div style={{ overflow: 'scroll' }}>
-        <AreaChart
-          width={500}
+        <LineChart
+        colors={['orange']}
+          xAxis={[{ scaleType: 'point', data: data.map(item => item.name) }]}
+          series={[{ data: data.map(item => item.value), area: true, showMark: false }]}
           height={400}
-          data={data}
-          margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip labelStyle={{ color: 'black' }}/>
-          <Area type="monotone" dataKey="value" stroke="#000" fill="#499ff5" />
-        </AreaChart>
-        </div>
+        />
       </Modal>
     </div>
   );
